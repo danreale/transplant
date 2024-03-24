ba9bec564dbd62bba055669a29d76e4254dba389
Dan Reale <danreale10@gmail.com>
2024-03-24 09:32:45 -0400

    added transformations

---
+++ headerTransformer.js
@@ -0,0 +1,34 @@
+const lineByLine = require("n-readlines");
+const liner = new lineByLine("ModifiedWaitingList.csv");
+const fs = require("fs");
+const path = require("path");
+
+let line;
+let lineNumber = 0;
+
+const fileContents = [];
+
+while ((line = liner.next())) {
+  if (lineNumber === 0) {
+    const str = `region,blood_type,empty,all_types,heart_status_1A,heart_status_1B,heart_status_2,heart_status_7_inactive`;
+    fileContents.push(str);
+  } else {
+    const liney = line.toString();
+    fileContents.push(liney);
+  }
+  lineNumber++;
+}
+
+console.log("end of file reached");
+console.log(fileContents.join("\n"));
+
+const file = path.join(__dirname, "..", "TransformedWaitingListHeader.csv");
+console.log(file);
+
+fs.writeFile(file, fileContents.join("\n"), (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log("Created new file successfully");
+  }
+});
---
+++ transformer.js
@@ -0,0 +1,52 @@
+const csv = require("csvtojson");
+const fs = require("fs");
+const path = require("path");
+
+async function transplantData() {
+  const jsonArray = await csv().fromFile("TransformedWaitingListHeader.csv");
+  //   console.log(jsonArray);
+
+  const transformedData = [];
+  let region = "";
+  for (let index = 0; index < jsonArray.length; index++) {
+    const element = jsonArray[index];
+    // console.log(region);
+    if (element.region !== "") {
+      // if has a region, set the region
+      region = element.region;
+      //   console.log(element);
+      element.all_types = parseInt(element.all_types);
+      element.heart_status_1A = parseInt(element.heart_status_1A);
+      element.heart_status_1B = parseInt(element.heart_status_1B);
+      element.heart_status_2 = parseInt(element.heart_status_2);
+      element.heart_status_7_inactive = parseInt(
+        element.heart_status_7_inactive
+      );
+      transformedData.push(element);
+    } else {
+      // if no region, then take the set region
+      element.region = region;
+      element.all_types = parseInt(element.all_types);
+      element.heart_status_1A = parseInt(element.heart_status_1A);
+      element.heart_status_1B = parseInt(element.heart_status_1B);
+      element.heart_status_2 = parseInt(element.heart_status_2);
+      element.heart_status_7_inactive = parseInt(
+        element.heart_status_7_inactive
+      );
+      transformedData.push(element);
+    }
+  }
+  // console.log(transformedData);
+  const file = path.join(__dirname, "..", "DatabaseWaitingList.json");
+  console.log(file);
+
+  fs.writeFile(file, JSON.stringify(transformedData, null, 2), (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("Created new file successfully");
+    }
+  });
+}
+
+transplantData();
