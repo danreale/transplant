750ef45c41f99e23225040cc3b28a6760a075c1f
Dan Reale <danreale10@gmail.com>
2024-03-24 18:49:04 -0400

    comments

--- app/data/db.server.ts
+++ app/data/db.server.ts
@@ -27,21 +27,6 @@ export async function getTransplantData(region: string, date: string) {
   return transplantData;
 }
 
-// export async function getTransplantDataWeek(wantedDate: string) {
-//   const formattedDate = DateTime.fromFormat(wantedDate, "yyyy-MM-dd").toISO();
-//   const transplantData = await getXataClient()
-//     .db.transplant_data.filter({
-//       report_date: formattedDate,
-//     })
-//     .getAll();
-//   return transplantData;
-
-//   // get first day of week data
-//   // get last day of week data
-
-//   // for each record, subtract last day of week minus the first day of the week data and show positive or negative trend
-// }
-
 export async function bloodTypeTotals(bloodType: "B" | "O", date: string) {
   // const formattedDate = DateTime.fromFormat(date, "yyyy-MM-dd").toUTC().toISO();
   console.log("Server Blood Type Date From Client", date);
