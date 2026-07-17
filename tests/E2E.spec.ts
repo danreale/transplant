8d7ddabd29ba8e4941397e3d445aea461e0da5d8
Dan Reale <danreale10@gmail.com>
2026-07-17 08:56:56 -0400

    fix tests

--- tests/E2E.spec.ts
+++ tests/E2E.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect, Page } from "@playwright/test";
+import { test, expect, type Page } from "@playwright/test";
 import { DateTime } from "luxon";
 import { getBaseUrl } from "playwright.config";
 import { regionStates } from "~/data/states";
